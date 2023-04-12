0457c764664f3648e31a70b3b3e2b4b3323db3c6
Chun Khai <chunkhai.k@gmail.com>
2023-04-12 18:36:01 +0800

    Completed procedure 1

--- 2. Procedures.sql
+++ 2. Procedures.sql
@@ -1,3 +1,63 @@
--- submit requests //TODO
+-- submit requests 
+    CREATE OR REPLACE PROCEDURE submit_request(
+    customer_id INTEGER, evaluator_id INTEGER, 
+    pickup_addr TEXT, pickup_postal TEXT, 
+    recipient_name TEXT, recipient_addr TEXT, 
+    recipient_postal TEXT, submission_time TIMESTAMP, 
+    package_num INTEGER, reported_height INTEGER[], 
+    reported_width INTEGER[], reported_depth INTEGER[], 
+    reported_weight INTEGER[], content TEXT[], 
+    estimated_value NUMERIC[]
+    ) AS $$ DECLARE request_id INTEGER;
+    package_id INTEGER;
+    BEGIN 
+        -- Insert delivery request
+        INSERT INTO delivery_requests (
+            customer_id, evaluater_id, status, 
+            pickup_addr, pickup_postal, recipient_name, 
+            recipient_addr, recipient_postal, 
+            submission_time) 
+        VALUES (
+            customer_id, evaluator_id, 'submitted', 
+            pickup_addr, pickup_postal, recipient_name, 
+            recipient_addr, recipient_postal, 
+            submission_time
+        ) RETURNING id INTO request_id;
+
+        -- Insert packages for the delivery request
+        FOR i IN 1..package_num LOOP 
+            INSERT INTO packages (
+                request_id, package_id, reported_height, 
+                reported_width, reported_depth, 
+                reported_weight, content, estimated_value
+                ) 
+            VALUES (
+                request_id, i, reported_height[i], 
+                reported_width[i], reported_depth[i], 
+                reported_weight[i], content[i], 
+                estimated_value[i]);
+        END LOOP;
+
+        -- Set actual dimensions to NULL for each package
+        UPDATE packages 
+        SET 
+            actual_height = NULL, 
+            actual_width = NULL, 
+            actual_depth = NULL, 
+            actual_weight = NULL 
+        WHERE 
+            request_id = request_id;
+        
+        -- Set pickup_date, num_days_needed, and price to NULL for the delivery request
+        UPDATE delivery_requests 
+        SET 
+            pickup_date = NULL, 
+            num_days_needed = NULL, 
+            price = NULL 
+        WHERE 
+            id = request_id;
+    END;
+    $$ LANGUAGE plpgsql;
+
 -- resubmit_requests //TODO
 -- insert_leg //TODO
