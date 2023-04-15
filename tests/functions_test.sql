25ec3fc2157652630b9ce1ce203dfe853455ced1
LianGuoYang <e0735382@u.nus.edu>
2023-04-15 10:20:42 +0800

    Function 1 Test: Add Test Case

--- tests/functions_test.sql
+++ tests/functions_test.sql
@@ -8,7 +8,82 @@
         alter sequence employees_id_seq restart with 1;
         alter sequence facilities_id_seq restart with 1;
 
--- view_trajectory //TODO
+-- view_trajectory
+
+    INSERT INTO customers (name, gender, mobile) VALUES
+     ('Chao Yung', 'male', '94235612'),
+     ('Kevin Chang', 'male', '92354278'),
+     ('Prittam Ravi', 'female', '82953156'),
+     ('Shawn Kok', 'female', '82856178'),
+     ('Jane Smith', 'female', '0987654321'),
+     ('Bob Johnson', 'male', '1112223333');
+
+    INSERT INTO employees(name, gender, dob, title, salary)
+    VALUES ('John Doe', 'male', '1990-01-01', 'Delivery Staff', 40000);
+    
+    INSERT INTO delivery_requests (customer_id, evaluater_id, status, pickup_addr, pickup_postal, recipient_name, 
+    recipient_addr, recipient_postal, submission_time, pickup_date, num_days_needed, price) VALUES 
+    (1, 1, 'submitted', 'PICKUP ADDR', '685687', 'Jon Kit', 
+    '23 Toa Payoh Drive', '434257', '2022-05-06 17:20:39',  '2022-05-07', 3, 5.00),
+    (2, 1, 'submitted', 'PICKUP ADDR', '685687', 'Jon Kit', 
+    '23 Toa Payoh Drive', '434257', '2022-05-06 17:20:39',  '2022-05-07', 3, 5.00),
+    (3, 1, 'submitted', 'PICKUP ADDR', '685687', 'Jon Kit', 
+    '23 Toa Payoh Drive', '434257', '2022-05-06 17:20:39',  '2022-05-07', 3, 5.00),
+    (4, 1, 'submitted', 'PICKUP ADDR', '685687', 'Jon Kit', 
+    '23 Toa Payoh Drive', '434257', '2022-05-06 17:20:39',  '2022-05-07', 3, 5.00),
+    (5, 1, 'submitted', 'PICKUP ADDR', '685687','Jon Kit', 
+    '23 Toa Payoh Drive', '434257', '2022-05-06 17:20:39',  '2022-05-07', 3, 5.00),
+    (6, 1, 'submitted', 'PICKUP ADDR', '685687', 'Jon Kit', 
+    '23 Toa Payoh Drive', '434257', '2022-05-06 17:20:39',  '2022-05-07', 3, 5.00);
+
+    INSERT INTO packages (
+        request_id, package_id, reported_height, reported_width, reported_depth,
+        reported_weight, content, estimated_value) VALUES (
+        1, 1, 10, 20, 30,
+        50, 'Clothes', 200);
+
+    INSERT INTO facilities (id, address, postal) VALUES
+    (1, '1F', '12345'),
+    (2, '2F', '67890'),
+    (3, '3F', '13579'),
+    (4, '4F', '46801'),
+    (5, '5F', '79135'),
+    (6, '6F', '02468'),
+    (7, '7F', '35791'),
+    (8, '8F', '68013'),
+    (9, '9F', '97531'),
+    (10, '10F', '02468');
+
+    INSERT INTO delivery_staff VALUES (1);
+
+    INSERT INTO accepted_requests (id, card_number, payment_time, monitor_id) VALUES 
+    (1, '1234567890', '2023-04-13 10:30:00', 1),
+    (2, '1234567890', '2023-04-13 10:30:00', 1),
+    (3, '1234567890', '2023-04-13 10:30:00', 1),
+    (4, '1234567890', '2023-04-13 10:30:00', 1),
+    (5, '1234567890', '2023-04-13 10:30:00', 1),
+    (6, '1234567890', '2023-04-13 10:30:00', 1);
+
+    INSERT INTO cancelled_or_unsuccessful_requests VALUES (4);
+
+    INSERT INTO legs (request_id,leg_id,handler_id,start_time,end_time,destination_facility) VALUES 
+    (1, 1, 1, '2023-04-12 16:00:00', '2023-04-12 16:00:01', 1) ,
+    (1, 2, 1, '2023-04-12 16:00:02', '2023-04-12 16:00:03', 1) ,
+    (1, 3, 1, '2023-04-12 16:00:04', '2023-04-12 16:00:05', 1) ,
+    (2, 4, 1, '2023-04-12 16:00:06', '2023-04-12 16:00:07', 9) ,
+    (2, 5, 1, '2023-04-12 16:00:08', '2023-04-12 16:00:09', 8) ,
+    (3, 6, 1, '2023-04-12 16:00:10', '2023-04-12 16:00:11', 2) ,
+    (3, 7, 1, '2023-04-12 16:00:12', '2023-04-12 16:00:13', 9) ,
+    (3, 8, 1, '2023-04-12 16:00:14', '2023-04-12 16:00:15', 4) ,
+    (3, 9, 1, '2023-04-12 16:00:16', '2023-04-12 16:00:17', 10);
+
+    INSERT INTO return_legs (request_id,leg_id,handler_id,start_time,end_time,source_facility) VALUES 
+    (4, 1, 1, '2023-04-12 16:01:00', '2023-04-12 16:02:00', 8),
+    (4, 2, 1, '2023-04-12 16:03:00', '2023-04-12 16:04:00', 1),
+    (4, 3, 1, '2023-04-12 16:05:00', '2023-04-12 16:06:00', 2),
+    (4, 4, 1, '2023-04-12 16:07:00', '2023-04-12 16:08:00', 1),
+    (4, 5, 1, '2023-04-12 16:09:00', '2023-04-12 16:10:00', 2);
+
 -- get_top_delivery_persons 
 
     INSERT INTO customers (name, gender, mobile) VALUES
@@ -78,4 +153,5 @@
     (6, 2, 5, '2023-04-12 15:29:00');
 
     SELECT * FROM get_top_delivery_persons(6)
+
 -- get_top_connections //TODO
